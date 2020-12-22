4cb7d9e9fb59ab56a3c1fa76ac98b661ffbd43ba
James <jamesgrubb@mac.com>
2020-12-22 00:15:26 +0000

    functions mutating

--- amplify/backend/function/amphandsd69716b1/src/index.js
+++ amplify/backend/function/amphandsd69716b1/src/index.js
@@ -1,14 +1,40 @@
 
 const fetch = require('node-fetch')
 exports.handler = async (event) => {
+    
     const {query} = JSON.parse(event.body)
-    const response = await{
+    
+
+    const updateCount = await fetch('https://handcount.hasura.app/v1/graphql',{
+        method:'POST',
+        statusCode:200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-type":"application/json"
+        },
+        body: JSON.stringify({
+            query:`
+            mutation MyMutation {
+                update_handcount(_inc: {hands: "${query}"}, where: {hands: {_gte: "0"}}) {
+                  returning {
+                    hands
+                  }
+                }
+              }
+              
+              
+            `,
+            variables: {}
+        }),
+    }).then(res => res.json()).catch(err => console.log('error from the response to hasura' + err))   
+
+    const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
      headers: {
          "Access-Control-Allow-Origin": "*"
      }, 
-        body: JSON.stringify(`Hello from Lambda with! ${query}`),
+        body: JSON.stringify(updateCount),
     };
     return response;
 };
